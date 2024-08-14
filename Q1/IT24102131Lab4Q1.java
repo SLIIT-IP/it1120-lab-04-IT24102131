5a864e661d32657830cfc148a82cd2071b01119b
IT24102131 <it24102131@my.sliit.lk>
2024-08-14 10:19:31 +0530

    Add files via upload

---
+++ Q1/IT24102131Lab4Q1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IT24102131Lab4Q1
+{ public static void main(String[]args)
+	{
+       Scanner input= new Scanner(System.in);
+	   int num;
+	   System.out.print("Enter a number :");
+	   num = input.nextInt();
+	   if ( num > 0)
+	   {System.out.print("The number is : positive");}
+	   else if( num == 0)
+	   {System.out.print("The number is Zero");}
+	   else
+	   {System.out.print("The number is : Negative");}
+	   }
+	   }
+	   
